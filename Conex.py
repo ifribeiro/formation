52d5b8fc4359be961feb5484a2b4ad2c32739b70
ifribeiro <ifreitasribeiro@hotmail.com>
2019-09-03 13:41:02 -0300

    Realiza chamada recursiva

--- Conex.py
+++ Conex.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import random
+from random import choice
 import numpy as np
+
 
 class Conex():
     def __init__(self):
@@ -10,11 +12,12 @@ class Conex():
 
 
     def initvariables(self):
-        self.V = ['a','b','c','d','e','f','g','h','i','j']
+        self.V_temp = ['a','b','c','d','e','f','g','h','i','j']
+        self.V = set(['a','b','c','d','e','f','g','h','i','j'])
         self.R = {}
         #    G =  a  b  c  d  e  f  g  h  i  j
         self.G =[[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-                 [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+                 [1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
                  [1, 1, 0, 1, 0, 0, 0, 0, 0, 0],
                  [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
@@ -26,36 +29,44 @@ class Conex():
                 ]
         
         for i in self.V:
-            self.R[i] = []
+            self.R[i] = set()
 
     def calcConex(self):
-        vertice = random.randint(0,9)
-        #v = V[i]
-        v = 'a'
-        self.R[v].append(v)
-        Y = []
+        v = choice(list(self.V))
+        print('v: ', v)
+        self.R[v].add(v)
+        Y = set()
+        
         #v = 'a'
-        difference = (set(self.neighboors(v)) - set(self.R[v]))
-        if (len(difference)==0):
-            print ('empty')
-        else:
-            print ("not empty")
+        while(len(self.neighboors(self.R[v]) - self.R[v])!=0):
+            Y = self.neighboors(self.R[v]) - self.R[v]
+            self.R[v] = self.R[v] | Y
+        
+        Y = self.R[v]
+        self.V = self.V - Y
+        print ("V: ", self.V)
+
+        """
+        if (len(self.V)!=0){
+            self.calcConex()
+        }"""
 
     """
     "v = Vertice
-    "Returns = Neighboors of v
+    "Returns = set of Neighboors of v
     """
     def neighboors(self,v):
-        pos = self.returnIndex(v) # position of vertice in array        
-        vertices = self.G[pos] # adjacency of vertice v
-        neighboors_vertices = [] # initializes vertices' array
-        for i in range(0,len(vertices)): 
-            if(vertices[i]==1):
-                neighboors_vertices.append(self.V[i])
+        neighboors_vertices = set() # initializes vertices' array
+        for i in v:            
+            pos = self.returnIndex(i) # position of vertice in array        
+            vertices = self.G[pos] # adjacency of vertice v            
+            for i in range(0,len(vertices)): 
+                if(vertices[i]==1):
+                    neighboors_vertices.add(self.V_temp[i])
         return neighboors_vertices
 
     def returnIndex(self, v):
-        return self.V.index(v)
+        return self.V_temp.index(v)
 if __name__ == '__main__':
     Conex = Conex()
     print (Conex.calcConex())
