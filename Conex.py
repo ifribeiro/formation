73a369951bb6734137e0a1c3384f2e3fa5f4302d
ifribeiro <ifreitasribeiro@hotmail.com>
2019-09-06 23:33:12 -0300

    Algumas correções

--- Conex.py
+++ Conex.py
@@ -30,43 +30,62 @@ class Conex():
         
         for i in self.V:
             self.R[i] = set()
+        self.conexComponet = []
 
+    """
+    " Return the conex components of a graph
+    """
     def calcConex(self):
+
+        #chooses a vertice of V
         v = choice(list(self.V))
-        print('v: ', v)
+
+        #add vertice v to dict R, with key 'v'
         self.R[v].add(v)
+
+        #initialize Y as a set
         Y = set()
-        
-        #v = 'a'
+
+        #while there's a neighboor vertice
         while(len(self.neighboors(self.R[v]) - self.R[v])!=0):
+            
+            #Calculate the neighboors of R(v) - R(v)
             Y = self.neighboors(self.R[v]) - self.R[v]
+
+            #R(v) = R(v) U Y
             self.R[v] = self.R[v] | Y
-        
+
+            #add R(V) to array conexComponent
+            self.conexComponet.append(list(self.R[v]))
+
+        #set as R(v)
         Y = self.R[v]
-        self.V = self.V - Y
-        print ("V: ", self.V)
 
-        """
-        if (len(self.V)!=0){
+        #set V as the difference between the sets V and Y
+        self.V = self.V - Y        
+        
+        #if there's vertices left in V
+        if (len(self.V)!=0):
             self.calcConex()
-        }"""
+        
+        return self.conexComponet
+        
 
     """
-    "v = Vertice
-    "Returns = set of Neighboors of v
+    " V = set of Vertices
+    " Returns = set of Neighboors of v
     """
-    def neighboors(self,v):
+    def neighboors(self,V):
         neighboors_vertices = set() # initializes vertices' array
-        for i in v:            
-            pos = self.returnIndex(i) # position of vertice in array        
+        for i in V:            
+            pos = self.V_temp.index(i) # position of vertice in array        
             vertices = self.G[pos] # adjacency of vertice v            
             for i in range(0,len(vertices)): 
                 if(vertices[i]==1):
                     neighboors_vertices.add(self.V_temp[i])
         return neighboors_vertices
-
-    def returnIndex(self, v):
-        return self.V_temp.index(v)
+    
 if __name__ == '__main__':
     Conex = Conex()
-    print (Conex.calcConex())
+    print("Componentes Conexas")
+    print(Conex.calcConex())
