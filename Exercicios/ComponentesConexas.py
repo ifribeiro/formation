3df896b0b9656e73c408a7ceb575962ea334548e
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2019-10-09 20:30:38 -0300

    Finalização atividade 1

--- Exercicios/ComponentesConexas.py
+++ Exercicios/ComponentesConexas.py
@@ -1,39 +1,130 @@
 import numpy as np
-from Conex import Conex
-grafos = []
-nTestes = int(input())
+import random
+from random import choice
+import numpy as np
+import string
 
-vertices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','k','r','s','t','u','v','x','z','w']
-print (vertices.index('a'))
-
-for grafo in range(0,nTestes):
-    print ("Vertice - Arestas")
-    nVert_Ares = input().split(" ")    
-    matrix = np.zeros((int(nVert_Ares[0]), int(nVert_Ares[0])))
-    grafos.append(matrix)
-    for j in range(0, int(nVert_Ares[1])):
-        print ("Vertice Vertice")
-        conexao = input().split(" ")
-        if (conexao[0] != ''):
-            idx1 = vertices.index(conexao[0])
-            idx2 = vertices.index(conexao[1])
-            grafos[grafo][idx1][idx2] = 1
-            grafos[grafo][idx2][idx1] = 1
-        else:
-            grafos[grafo][idx1][idx2] = 0
+class Conex():
+    def __init__(self, G):
+        self.initvariables(G) # inicializacao de variaveis
 
 
+    def initvariables(self, G):
+        self.V_temp = list(string.ascii_lowercase)[:len(G)]
+        self.V = set(self.V_temp)
+        self.R = {}
+        self.G = G
+        
+        for i in self.V:
+            self.R[i] = set()
+        self.conexComponet = []
 
-for i in range(0,len(grafos)):
-    print ("Case #%s:"%(i+1))
-    c = Conex(grafos[i])
-    listConex = (c.calcConex()) #ordena as componentes conexas
-    listConex = [sorted(x) for x in listConex]
-    listConex = sorted(listConex)
-    for component in listConex:
-        component = sorted(component)
-        print (",".join(component)+",")
-    print ("%s connected components\n\n"%(len(listConex)))
+    """
+    " Return the conex components of a graph
+    """
+    def calcConex(self):
+
+        #chooses a vertice of V
+        v = choice(list(self.V))
+        
+
+        #add vertice v to dict R, with key 'v'
+        self.R[v].add(v)
+        
+        #initialize Y as a set
+        Y = set()
+
+        #while there's a neighboor vertice
+        while(len(self.neighboors(self.R[v]) - self.R[v])!=0):           
+            
+            #Calculate the neighboors of R(v) - R(v)
+            Y = self.neighboors(self.R[v]) - self.R[v]
+
+            #R(v) = R(v) U Y
+            self.R[v] = self.R[v] | Y
+
+            #add R(V) to array conexComponent
+            #Garante que apenas o maximal será adicionado            
+            for set_vertices in self.conexComponet:                
+                if (len(set_vertices)<len(list(self.R[v]))):                
+                    if(all([x in list(self.R[v]) for x in set_vertices ])):
+                        self.conexComponet.remove(set_vertices)                   
+            
+            
+        self.conexComponet.append(list(self.R[v]))
+        #set as R(v)
+        Y = self.R[v]
+        
+
+        #set V as the difference between the sets V and Y
+        self.V = self.V - Y        
+        #if there's vertices left in V
+        if (len(self.V)!=0):
+            self.calcConex()
+        
+        return self.conexComponet
+        
+
+    """
+    " V = set of Vertices
+    " Returns = set of Neighboors of v
+    """
+    def neighboors(self,V):
+        neighboors_vertices = set() # initializes vertices' array
+        for i in V:            
+            pos = self.V_temp.index(i) # position of vertice in array
+            neighboors_vertices.add(self.V_temp[pos]) #adiciona o próprio vertice na lista de vizinhos
+            vertices = self.G[pos] # adjacency of vertice v            
+            for i in range(0,len(vertices)): 
+                if(vertices[i]==1):
+                    neighboors_vertices.add(self.V_temp[i])
+        return neighboors_vertices
+
+class ConnectedComponents():
+    def __init__(self):
+        self.grafos = []
+        self.vertices = list(string.ascii_lowercase)
+
+    def createMatrices(self, nTestes):
+        for grafo in range(0,nTestes):
+            nVert_Ares = input().split(" ")    
+            matrix = np.zeros((int(nVert_Ares[0]), int(nVert_Ares[0])))
+            self.grafos.append(matrix)
+            for j in range(0, int(nVert_Ares[1])):
+                conexao = input().split(" ")
+                if (conexao[0] != ''):
+                    idx1 = self.vertices.index(conexao[0])
+                    idx2 = self.vertices.index(conexao[1])
+                    self.grafos[grafo][idx1][idx2] = 1
+                    self.grafos[grafo][idx2][idx1] = 1
+                else:
+                    self.grafos[grafo][idx1][idx2] = 0
+
+    def calcConnectedComponents(self):
+        for i in range(0,len(self.grafos)):
+            print ("Case #%s:"%(i+1))
+            c = Conex(self.grafos[i])
+            listConex = (c.calcConex()) #ordena as componentes conexas
+            listConex = [sorted(x) for x in listConex]
+            listConex = sorted(listConex)
+            for component in listConex:
+                component = sorted(component)
+                print (",".join(component)+",")
+            print ("%s connected components\n\n"%(len(listConex)))
+    
+if __name__ == "__main__":
+    CC = ConnectedComponents()
+    nTestes = int(input())
+    CC.createMatrices(nTestes)
+    print ("\n\n")
+    CC.calcConnectedComponents()
+
+
+
+
+
+
+
 
 
 
